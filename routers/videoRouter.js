368573911dc75bd4c214d98131ac23d577c64a1d
funkyblues <dnr8874@naver.com>
2020-03-27 16:58:49 +0900

    #2-14 commit

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
+import { upload, videoDetail, editVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
 
 export default videoRouter;
